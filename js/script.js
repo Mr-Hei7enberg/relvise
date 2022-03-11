3328767cc57a84ae76955e7be17b2c08e16249b4
Mr-Hei7enberg <shebanits123456@gmail.com>
2022-03-11 19:33:47 +0200

    header
    add fixed header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-"use strict"
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+        document.getElementById("header").classList.add("header_fixed");
+    } else {
+        document.getElementById("header").classList.remove("header_fixed");
+    }
+}
